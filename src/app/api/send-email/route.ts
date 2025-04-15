9261ae52359a82ca09201c5853360b05b8fb9b10
SamarpanKc <samarpan0kc@gmail.com>
2025-04-15 06:18:00 +0545

    mail update colors

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -32,14 +32,14 @@ export async function POST(request: Request) {
       subject: "Workshop Registration Confirmation",
       text: `Hello ${firstName} ${lastName},\n\nThank you for registering for our workshop! We have received your registration and we're excited to have you join us.\n\nWorkshop Date: [Insert Workshop Date Here]\nWorkshop Time: 7:00 to 8:00 PM\nMeeting Link: [Insert Meeting Link Here]\nOur Facebook Page: [Insert Facebook Page Link Here]\n\nBest regards,\nTRI (Tech Research and Innovation)`,
       html: `
-        <table width="100%" cellpadding="0" cellspacing="0" border="0" style="font-family: Arial, sans-serif; font-size: 16px; color: #f7f5fb;">
+    <table width="100%" cellpadding="0" cellspacing="0" border="0" style="font-family: Arial, sans-serif; font-size: 16px; color: #f7f5fb;">
       <tr>
         <td align="center" style="padding: 20px 0;">
           <table cellpadding="0" cellspacing="0" border="0" style="max-width: 800px; width: 100%; background-color: none; border: 1px solid #ddd; border-radius: 5px;">
             <tr>
               <td align="left" style="padding: 30px;">
                 <h2 style="color: #001b45; margin-top: 0; margin-bottom: 34px; font-size: 24px; text-align: center;">TRI Workshop Registration Confirmation</h2>
-                <p style="margin-top: 0; margin-bottom: 15px; text-align: left;">Hello <strong style="font-weight: bold; color: #001b45">${firstName} ${lastName}</strong>,</p>
+                <p style="margin-top: 0; margin-bottom: 15px; text-align: left; color: #001b45;">Hello <strong style="font-weight: bold; color: #001b45;">${firstName} ${lastName}</strong>,</p>
                 <p style="margin-top: 0; margin-bottom: 20px; line-height: 1.5; text-align: left;">Thank you for registering for our workshop! We have received your registration and we're excited to have you join us.</p>
 
                 <p style="margin-top: 0; margin-bottom: 10px; text-align: left;">
